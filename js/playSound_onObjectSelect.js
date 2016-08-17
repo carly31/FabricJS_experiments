13d049d72df50cf9125689a54b838a6dc3e8249c
Carla Panda <panda_oku@yahoo.com>
2016-08-17 09:15:59 +0800

    Create playSound_onObjectSelect.js

---
+++ js/playSound_onObjectSelect.js
@@ -0,0 +1,36 @@
+
+//::START
+var canvas = new fabric.Canvas('can', {
+  backgroundColor: 'rgb(255,255,255)',
+  selectionColor: 'lightgray',
+  selectionLineWidth: 2,
+  selection: false
+});
+
+
+//Sound
+var Sound = {
+    playMove: function() {
+        var sound = new Audio('../media/move.wav');
+        sound.play();
+    }
+};
+
+var circle = new fabric.Circle({
+  radius: 20, fill: 'green', left: 100, top: 100
+});
+
+canvas.add(circle);
+
+
+circle.on('selected', function(options) {
+	Sound.playMove();
+});
+
+/*
+canvas.on('mouse:down', function(options) {
+  
+	Sound.playMove();
+
+});
+*/
